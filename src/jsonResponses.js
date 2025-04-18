4788f93e2b5bbb5d1ae37bd6d1f5159243b7eac8
Mozzy Stix <cad8682@g.rit.edu>
2025-04-18 13:56:42 -0400

    Got some issues

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,73 +1,274 @@
+const { randomInt } = require('node:crypto');
+const fs = require('node:fs');
+
+const respondJSON = (request, response, status, object) => {
+    const content = JSON.stringify(object);
+    response.writeHead(status, {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(content, 'utf8'),
+    });
+  
+    if (request.method !== 'HEAD' && status !== 204) {
+      response.write(content);
+    }
+    
+    response.end();
+  };
+
 //searches the Pokedex by checking what mons fit the given parameters. If no parameters given, whole dex
 const searchPokedex = (request, response) => {
-    //
+    let data;
+    try {
+        data = fs.readFileSync('./data/pokedex.json', 'utf8');
+        data = JSON.parse(data);
+    } catch (err) {
+        console.error(err);
+        const responseData = {
+            message: 'Server Error When Reading Dataset',
+            id: 'Id: internalError'
+        };
+        respondJSON(request, response, 500, responseData);
+        return;
+    }
+
+    // filter by name
+    if (request.query.name) {
+        data = data.filter(pokeman => {
+            return pokeman.name.toLowerCase() === request.query.name.toLowerCase();
+        });
+    }
+    // filter by number
+    //Not working
+    if (request.query.number) {
+        data = data.filter(pokeman => {
+            return pokeman.id === Number(request.query.number);
+        });
+    }
+    // filter by type one
+    if (request.query.type1) {
+        data = data.filter(pokeman => {
+            return pokeman.type[0].toLowerCase() === request.query.type1.toLowerCase();
+        });
+    }
+    // filter by type two
+    if (request.query.type2) {
+        data = data.filter(pokeman => {
+            if (pokeman.type.length > 1) {
+                return pokeman.type[1].toLowerCase() === request.query.type2.toLowerCase();
+            }
+        });
+    }
+    // filter by minimum weight The JSON has kgs and ms attached to the heights and weights
+    if (request.query.weightMin) {
+        data = data.filter(pokeman => {
+            return pokeman.weight >= Number(request.query.weightMin);
+        });
+    }
+    // filter by maximum weight
+    if (request.query.weightMax) {
+        data = data.filter(pokeman => {
+            return pokeman.weight <= Number(request.query.weightMax);
+        });
+    }
+    // filter by minimum height
+    if (request.query.heightMin) {
+        data = data.filter(pokeman => {
+            return pokeman.height >= Number(request.query.heightMin);
+        });
+    }
+    // filter by maximum height
+    if (request.query.heightMax) {
+        data = data.filter(pokeman => {
+            return pokeman.height <= Number(request.query.heightMax);
+        });
+    }
+    // filter by given weakness
+    if (request.query.weakness) {
+        data = data.filter(pokeman => {
+            return pokeman.weaknesses.find(weakness => {
+                 return request.query.weakness.toLowerCase() === weakness.toLowerCase();
+            } ) !== undefined;
+        });
+    }
+
+    //Fix handlePost, use RespondJSON
+    respondJSON(request, response, 200, data);
 };
 
 //gets all Pokemon of the user's given type
-const getType = (request, response) => {
-    //
-};
+const getTypes = (request, response) => {
+    let data;
+    try {
+        data = fs.readFileSync('./data/pokedex.json', 'utf8');
+        data = JSON.parse(data);
+    } catch (err) {
+        console.error(err);
+        const responseData = {
+            message: 'Server Error When Reading Dataset',
+            id: 'Id: internalError'
+        };
+        respondJSON(request, response, 500, responseData);
+        return;
+    }
 
-//gets the shiny of a user given name or id
-const getShiny = (request, response) => {
-    //
-};
+    if (request.data.type1 || request.data.type2) {
+        // filter by type one
+        if (request.query.type1) {
+            data = data.filter(pokeman => {
+                return pokeman.type[0].toLowerCase() === request.query.type1.toLowerCase();
+            });
+        }
+        // filter by type two
+        if (request.query.type2) {
+            data = data.filter(pokeman => {
+                if (pokeman.type.length > 1) {
+                    return pokeman.type[1].toLowerCase() === request.query.type2.toLowerCase();
+                }
+            });
+        }
 
-//gets all the costume images. If no parameters, every?
-const getCostumes = (request, response) => {
-    //
+        respondJSON(request, response, 200, data);
+    }
+    else {
+        const responseData = {
+            message: 'Error: Please input a primary and or secondary type',
+            id: 'Id: badRequest'
+        };
+        respondJSON(request, response, 400, responseData);
+        return;
+    }
 };
 
 //adds a Pokemon, needs every parameter filled, else give status 400
 const addPokemon = (request, response) => {
-    //
-};
+    let newPokemon;
+    const responseJSON = {
+      message: 'All fields are required.',
+    };
+  
+    const { name, number, typeOne, typeTwo, weightMin, weightMax, heightMin, heightMax, weakness } = request.body;
+  
+    if (!name || !number || !typeOne || !typeTwo || !weightMin || !weightMax || !heightMin || !heightMax || !weakness) {
+      responseJSON.id = 'missingParams';
+      return respondJSON(request, response, 400, responseJSON);
+    }
+    
+    let data;
+    try {
+        data = fs.readFileSync('./data/pokedex.json', 'utf8');
+        data = JSON.parse(data);
+    } catch (err) {
+        console.error(err);
+        const responseData = {
+            message: 'Server Error When Reading Dataset',
+            id: 'Id: internalError'
+        };
+        respondJSON(request, response, 500, responseData);
+        return;
+    }
 
-//removes a given ID's Pokemon from the dex
-const removePokemon = (request, response) => {
-    //
-};
+    let responseCode = 204;
+  
+    if (!newPokemon[name]) {
+      responseCode = 201;
+      newPokemon[name] = {
+        name: name,
+      };
+    }
+    
+    newPokemon[name].number = number;
+    newPokemon[name].typeOne = typeOne;
+    newPokemon[name].typeTwo = typeTwo;
+    newPokemon[name].weightMin = weightMin;
+    newPokemon[name].weightMax = weightMax;
+    newPokemon[name].heightMin = heightMin;
+    newPokemon[name].heightMax = heightMax;
+    newPokemon[name].weakness = weakness;
+  
+    data.push(newPokemon);
 
-//DO I KEEP? searches for a mega evolution of a user given name, returns the image
-const getMegas = (request, response) => {
-    //
-};
+    try {
+        data = fs.writeFileSync('./data/pokedex.json', 'utf8');
+        data = JSON.parse(data);
+    } catch (err) {
+        console.error(err);
+        const responseData = {
+            message: 'Server Error When Writing Dataset',
+            id: 'Id: internalError'
+        };
+        respondJSON(request, response, 500, responseData);
+        return;
+    }
 
-//DO I KEEP? the megas function but with the gigantamax value (How should I combine them into 1)
-const getGigantamax = (request, response) => {
-    //
+    //What's going on over here??
+    if (responseCode === 201) {
+      responseJSON.message = 'Created Successfully';
+      responseJSON.newMon = newPokemon[name];
+      return respondJSON(request, response, responseCode, responseJSON);
+    }
+    return respondJSON(request, response, responseCode, {});
 };
 
 //Takes a user given number and generates that many random Pokemon
 const getRandom = (request, response) => {
-    //
+    let data;
+    try {
+        data = fs.readFileSync('./data/pokedex.json', 'utf8');
+        data = JSON.parse(data);
+    } catch (err) {
+        console.error(err);
+        const responseData = {
+            message: 'Server Error When Reading Dataset',
+            id: 'Id: internalError'
+        };
+        respondJSON(request, response, 500, responseData);
+        return;
+    }
+
+    // using the number given, generate that many random Pokemon
+    if (request.query.number > 0) {
+        let generatedPokemon = [];
+        let randNum;
+        for (let i = 0; i < request.query.number; i++) {
+            randNum = Math.floor((Math.random() * data.length));
+
+            while (generatedPokemon.includes(randNum)) {
+                randNum = Math.floor((Math.random() * data.length));
+            }
+
+            generatedPokemon.push(randNum);
+        }
+
+        //Replace all the numbers with the mons
+        for (let i = 0; i < generatedPokemon.length; i++) {
+            generatedPokemon[i] = data[generatedPokemon[i]];
+            //console.log(generatedPokemon[i]);
+        }
+
+        respondJSON(request, response, 200, generatedPokemon);
+    }
+    else {
+        const responseData = {
+            message: 'Error: Need a positive number in the number field',
+            id: 'Id: badRequest'
+        };
+        respondJSON(request, response, 400, responseData);
+        return;
+    }
 };
 
 const notFound = (request, response) => {
-    const body = {
-      error: 'That content wasn\'t found'
-    }
-    const content = JSON.stringify(body);
-  
-    response.writeHead(404, { 
-      'Content-Type': 'application/json', 
-      'Content-Length': Buffer.byteLength(content, 'utf8'),
-    });
-  
-    response.write(body);
-  
-    response.end();
+    const responseData = {
+        message: 'Message: The page you were looking for cannot be found',
+        id: 'Id: notFound'
+      };
+        respondJSON(request, response, 404, responseData);
 };
 
 module.exports = {
     searchPokedex,
-    getType,
-    getShiny,
-    getCostumes,
-    getMegas,
-    getGigantamax,
+    getTypes,
     getRandom,
     addPokemon,
-    removePokemon,
     notFound
 };
--- src/server.js
+++ src/server.js
@@ -5,53 +5,69 @@ const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-//searchPokedex, getType, getShiny, getCostumes, addPokemon, removePokemon (by id, not name), getMegas? getGigantamax? getRandom
+const parseBody = (request, response, handler) => {
+  const body = [];
 
-const handlePost = async (request, response, parsedUrl) => {
-  const body = await parseBody(request);
-  request.body = body;
-  if (parsedUrl.pathname === '/addUser') {
-    //jsonHandler.addUser(request, response);
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    request.body = query.parse(bodyString);
+    handler(request, response);
+  });
+};
+
+// handle POST requests
+const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/addPokemon') {
+    parseBody(request, response, jsonHandler.addPokemon);
   }
 };
 
 const handleGet = (request, response, parsedUrl) => {
+  console.log(parsedUrl.pathname);
   if (parsedUrl.pathname === '/style.css') {
     htmlHandler.getCSS(request, response);
-  } else if (parsedUrl.pathname === '/getUsers') {
-    //jsonHandler.getUsers(request, response);
+  } else if (parsedUrl.pathname === '/searchPokedex') {
+    jsonHandler.searchPokedex(request, response);
   } else if (parsedUrl.pathname === '/') {
     htmlHandler.getIndex(request, response);
-  } else if (parsedUrl.pathname === '/addUser') {
-    //jsonHandler.addUser(request, response);
+  } else if (parsedUrl.pathname === '/getTypes') {
+    jsonHandler.getTypes(request, response);
+  } else if (parsedUrl.pathname === '/getRandom') {
+    jsonHandler.getRandom(request, response);
   }
   else {
-    //jsonHandler.notFound(request, response);
+    jsonHandler.notFound(request, response);
   }
 };
 
-const handleHead = (request, respoinse, parsedUrl) => {
+//const handleHead = (request, respoinse, parsedUrl) => {
     //
-};
+//};
 
 const onRequest = (request, response) => {
     const protocol = request.connection.encrypted ? 'https' : 'http';
     const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
-  
-        if (request.method === 'GET') {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
+
+        if (request.method === 'GET' || request.method === 'HEAD') {
           handleGet(request, response, parsedUrl);
         }
         if (request.method === 'POST') {
           handlePost(request, response, parsedUrl);
         }
-        if (request.method === 'HEAD') {
-            handleHead(request, response, parsedUrl);
-        }
-        response.writeHead(200, { 
-          'Content-Type': 'application/json', 
-          //Something with the parsedURL here is weird, I don't remember what the right variable is
-          //'Content-Length': Buffer.byteLength(parsedUrl, 'utf8'),
-        });
+        //if (request.method === 'HEAD') {
+            //handleHead(request, response, parsedUrl);
+        //}
   };
   
   http.createServer(onRequest).listen(port, () => {
